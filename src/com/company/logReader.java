fd0d12b2678a9b77a2b0f17992803407e76f328d
rececoffin <rcoffin@mymail.mines.edu>
2017-10-13 13:17:46 -0600

    Added .jar file

--- src/com/company/logReader.java
+++ src/com/company/logReader.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-
 import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
 /**
